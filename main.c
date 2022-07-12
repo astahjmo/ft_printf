6c69c29e35960824af84d1018826e88e2af36348
astahjmo <contato.johnatan06@gmail.com>
2022-07-12 11:17:11 -0300

    Remove main.c

--- main.c
+++
@@ -1,9 +0,0 @@
-#include "include/ft_printf.h"
-
-int main()
-{
-	char *str = "Eu amo a minami";
-	ft_printf("Memory address:%p\nString:%s\n%d\n%i\n%u\nHEX:%X\nhex:%x", str, str,10000,182738972, -1, 182738972, 182738972);
-	printf("\nAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-	printf("\nMemory address:%p\nString:%s\n%d\n%i\n%u\nHEX%X\nhex:%x", str, str, 10000,182738972, -1, 182738972,182738972);
-}
